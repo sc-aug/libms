290e74983b74b7536da263e0a68d893445af55d2
sc-aug <shichuanwang@yahoo.com>
2017-08-07 13:02:23 -0500

    update

--- build.sh
+++ build.sh
@@ -12,11 +12,14 @@ CV="/root/repo/"
 LV="/Users/chuan/myspace/docker/lib/"
 
 # local & container expose
-LP="8080"
-CP="8080"
+# ports for testing
+LP1="7000"
+CP1="7000"
+LP2="8000"
+CP2="8000"
 
 # db container name
 DBN="mymongo"
 
 #docker run -it -p $LP:$CP -v $LV:$CV --link $DBN --name $CN -h $CHN $IN "/bin/bash"
-docker run -it -p $LP:$CP -v $LV:$CV --name $CN -h $CHN $IN "/bin/bash"
+docker run -it -p $LP1:$CP1 -p $LP2:$LP2 -v $LV:$CV --name $CN -h $CHN $IN "/bin/bash"
