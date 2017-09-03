3c7baef84e48d21c9fb1520d4969b8a62e60d62d
sc-aug <shichuanwang@yahoo.com>
2017-09-03 10:53:03 -0500

    update config

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ mongoose.connect(uri, opt, function(err) {
 // CORS middleware
 const allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
